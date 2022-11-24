676b4ee231f20b1333b2288ebbcf2d14dc8ced34
tony <tony@computer.com>
2022-11-24 15:05:30 +0100

    reverted base_model file

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -21,7 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
         else:
             for k, v in kwargs.items():
@@ -35,7 +35,7 @@ class BaseModel:
     def save(self):
         """ Updates the public instance attribute `updated_at`"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """ Returns a dic containing all items of __dict__ of the instance.
